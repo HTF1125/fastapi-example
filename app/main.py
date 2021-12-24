b7426175438a5e92b8af9b86e9fc132a5bff9443
HTF1125 <roberthan1125@gmail.com>
2021-12-24 15:29:31 +0900

    fixed some settings

--- app/main.py
+++ app/main.py
@@ -1,32 +1,32 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from . import models
+from .database import engine
 from .router import post, user, auth, vote
-from . import database, models
-from sqlalchemy.orm import Session
+from .config import settings
 
 
-orgins = ["*"]
-
-# Create all the tables
-models.Base.metadata.create_all(bind=database.engine)
+# models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-app.add_middleware(CORSMiddleware, 
-                   allow_orgins=orgins,
-                   allow_credentials=True,
-                   allow_methods = ["*"],
-                   allow_headers=["*"])
+origins = ["*"]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(vote.router)
 
-@app.post("/delete")
-def delete_all_tables(db: Session =Depends(database.get_db)):
-    models.Base.metadata.drop_all(bind=database.engine)
-    return "Dropped all tables"
 
+@app.get("/")
+def root():
+    return {"message": "Hello World pushing out to ubuntu"}
