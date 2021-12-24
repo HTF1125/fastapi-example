b39b37e405cbdf09b69753184cf456452caeb6a5
HTF1125 <roberthan1125@gmail.com>
2021-12-24 16:11:07 +0900

    hello

--- app/router/auth.py
+++ app/router/auth.py
@@ -7,7 +7,7 @@ router = APIRouter(
     tags = ['Authentication']
 )
 
-@router.get('/login')
+@router.post('/login')
 def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
     
     user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
